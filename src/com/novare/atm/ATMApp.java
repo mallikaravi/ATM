71fe039c1d8bf8ccd0111a280a124affa6b00211
mallikaravi <mailtomallikaravi@gmail.com>
2022-08-29 19:44:33 +0200

    Added gitignore file

---
+++ src/com/novare/atm/ATMApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.novare.atm;
+
+/**
+ * @author malli
+ *
+ */
+public class ATMApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
