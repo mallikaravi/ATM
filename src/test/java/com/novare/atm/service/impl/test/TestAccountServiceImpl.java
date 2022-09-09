f2ecbf6e759b7f13deb1514c2ff1089b033ba791
mallikaravi <mailtomallikaravi@gmail.com>
2022-09-09 14:01:19 +0200

    Updated structure as per Guidelines and created Junit Test class

---
+++ src/test/java/com/novare/atm/service/impl/test/TestAccountServiceImpl.java
@@ -0,0 +1,49 @@
+package com.novare.atm.service.impl.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestAccountServiceImpl {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDeleteUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdatePassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsValidUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindByUserName() {
+		fail("Not yet implemented");
+	}
+
+}
